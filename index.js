4152eccf5b90800b212e10fb362db214b5f14a9c
rajesh-kumar-jashti <rajeshkumarjashti@gmail.com>
2024-05-08 23:16:18 +0530

    first Commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
 require("dotenv").config();
+const BASE_URL = process.env.BASE_URL
 
 
 app.use(express.json())
@@ -36,7 +37,7 @@ app.use('/images', express.static('upload/images'))
 app.post('/upload', upload.single('product'),(req,res)=>{
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `${BASE_URL}/images/${req.file.filename}`
     })
 })
 
