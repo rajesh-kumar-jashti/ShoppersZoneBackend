169190a26c8ff629adb2c314f7dbd83cbbd855f4
rajesh-kumar-jashti <rajeshkumarjashti@gmail.com>
2024-05-08 23:35:23 +0530

    final

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/images', express.static('upload/images'))
 app.post('/upload', upload.single('product'),(req,res)=>{
     res.json({
         success: 1,
-        image_url: `${BASE_URL}/images/${req.file.filename}`
+        image_url: `https://shopperszonebackend.onrender.com/images/${req.file.filename}`
     })
 })
 
